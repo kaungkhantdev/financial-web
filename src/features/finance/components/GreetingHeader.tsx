a7af493510d7ddbcd3741745b917c054c77a3f79
kaungkhantcodigo <kaungkhantzaw@codigo.co>
2025-10-21 16:59:31 +0700

    remove unnecessary margin

--- src/features/finance/components/GreetingHeader.tsx
+++ src/features/finance/components/GreetingHeader.tsx
@@ -3,7 +3,7 @@ import { Greeting } from '@/utils/helpers/greeting';
 
 export const GreetingHeader = () => (
   <div className="md:col-span-5 flex justify-between items-center">
-    <h1 className="text-lg font-medium mb-2">{Greeting()}, Jaylon</h1>
+    <h1 className="text-lg font-medium">{Greeting()}, Jaylon</h1>
     <div className="space-x-4 text-gray-500">
       <ChartSpline className="w-4 h-4 inline-block" />
       <BellDot className="w-4 h-4 inline-block" />

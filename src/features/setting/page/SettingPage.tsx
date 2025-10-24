e761268e0f969d19a64c0c1e0d0893a60058125b
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2025-10-24 11:57:26 +0700

    fix design

--- src/features/setting/page/SettingPage.tsx
+++ src/features/setting/page/SettingPage.tsx
@@ -3,9 +3,11 @@ import Basics from "../components/Basics"
 
 const SettingPage = () => {
   return (
-    <div className="md:p-8 lg:py-6 lg:pl-0 lg:pr-6 2xl:flex items-center justify-center w-full 2xl:min-h-screen">
-      <WalletHeader title="Settings" />
-      <Basics />
+    <div className="md:p-8 lg:py-6 lg:pl-0 lg:pr-6 2xl:flex flex-col items-center justify-center w-full 2xl:min-h-screen">
+      <div className="2xl:min-w-7xl">
+        <WalletHeader title="Settings" />
+        <Basics />
+      </div>
     </div>
   )
 }
